ec303f04bff2f609ddf506b833a60c91879a8b5a
giusepperagusa <giuseppe.ragusa@hotmail.com>
2015-08-10 01:18:45 +0200

    Create it.js

---
+++ l10n/it.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+"disclaimer",
+{
+"Disclaimer":
+"Informativa",
+"Current disclaimer path:":
+"Percorso attuale dell'informativa:",
+"Please, add a disclaimer file!":
+"Si prega di aggiungere un'informativa!",
+"Please, change the default disclaimer file!":
+"Si prega di modificare l'informativa predefinita!",
+"I have read and agree to the Terms & Conditions.":
+"Ho letto ed accetto i termini e le condizioni."
+},
+"nplurals=2; plural=(n != 1);");
