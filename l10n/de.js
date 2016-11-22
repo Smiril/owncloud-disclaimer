b8fecd80885a3c64193ba3388d0add2c12341cf3
Smiril <Smiril@users.noreply.github.com>
2016-11-22 20:23:21 +0100

    Add files via upload

---
+++ l10n/de.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+"disclaimer",
+{
+    "Disclaimer":
+    "Disclaimer",
+	
+	"Current disclaimer path:":
+	"Pfad zur Disclaimer Datei:",
+	
+	"Please, add a disclaimer file!":
+	"Bitte ein Disclaimer File anlegen!",
+	
+	"Please, change the default disclaimer file!":
+	"Bitte Standart Disclaimer File ersetzen!",
+	
+	"I have read and agree to the Terms & Conditions.":
+	"Ich habe das Disclaimer gelesen und akzeptiere die Vereinbarungen."
+
+},
+"nplurals=2; plural=(n != 1);");
