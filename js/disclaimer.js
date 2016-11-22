fdae817acec93938f4fa01acf4ec73e81c175159
Smiril <Smiril@users.noreply.github.com>
2016-11-22 20:20:15 +0100

    Update disclaimer.js

--- js/disclaimer.js
+++ js/disclaimer.js
@@ -1,21 +1,19 @@
 (function() {
-    
 var saml = document.createElement('script');
 saml.type = 'text/javascript';
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(saml);
 })();
 
 $(document).ready(function(){
-	$('#password').parent().removeClass("infield groupbottom");
-	$('#password').parent().addClass("infield groupmiddle");
-	$('#password').attr( "autocomplete", "on" );
-	$('#password').parent().after(
-		'<p class="infield disclaimer">'+
-		'<input type="checkbox" id="disclaimer" placeholder="Disclaimer" value="" name="disclaimer"'+ 'original-title="" autocomplete="off" required />'+
-		'<a href="apps/disclaimer/pdf/disclaimer.pdf "><img src="core/img/filetypes/application-pdf.svg" /></a> ' + t('disclaimer','I have read and agree to the Terms & Conditions.') +
-		'</p>'
-	);
+        $('#password').parent().removeClass("infield groupbottom");
+        $('#password').parent().addClass("infield groupmiddle");
+        $('#password').attr( "autocomplete", "on" );
+        $('#password').parent().after(
+                '<p class="infield disclaimer">'+
+                '<input type="checkbox" id="disclaimer" placeholder="Disclaimer" value="" name="disclaimer"'+ 'original-title="" autocomplete="off" required />'+
+                '<a href="/apps/disclaimer/pdf/disclaimer.pdf" target="_blank"><img src="/apps/disclaimer/img/application-pdf.png" width="30" height="35"/></a> ' + t('disclaimer','I have read and agree to the Terms & Conditions.') +
+                '</p>'
+        );
 
     $("#submit").removeAttr("disabled");
 });
-
