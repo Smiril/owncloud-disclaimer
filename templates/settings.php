609ae0310c9d0eeef737b29f699a60e672a9bee6
Smiril <Smiril@users.noreply.github.com>
2016-11-23 12:47:30 +0100

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -1,19 +1,31 @@
 <?php //use \OCA\disclaimer\disclaimer; ?>
 <form id="disclaimer">
-	<div class="section">
-		<h2><?php p($l->t('Disclaimer'));?></h2>
-		<p><?php p($l->t('Current disclaimer path:')); ?> <a href="/apps/disclaimer/pdf/disclaimer.pdf"><?php echo realpath(dirname(__DIR__));?>/pdf/disclaimer.pdf</a></p>
-		
-		<?php if(!is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){?>
-			<span class="connectionwarning">
-				<?php p($l->t("Please, add a disclaimer file!"));?>
-			</span>
-		<?php } ?>
-		<?php if((is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")) && 
-				(md5_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf") === "54b3422935ea1305b2904bb6b906e095")){?>
-			<span class="connectionwarning">
-				<?php p($l->t("Please, change the default disclaimer file!"));?>
-			</span>
-		<?php } ?>
-	</div>
+        <div class="section">
+                <h2><?php p($l->t('Disclaimer'));?></h2>
+                <p><?php p($l->t('Current disclaimer path:')); ?> <a href="/apps/disclaimer/pdf/disclaimer.pdf" target="_blank"><?php echo realpath(dirname(__DIR__));?>/pdf/disclaimer.pdf</a></p>
+                <form enctype="multipart/form-data" method="post" class="upload">
+                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <input type="submit" name="save" value="Upload" id="save" />
+                </form>
+
+                        <?php if(!is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){?>
+                        <span class="connectionwarning">
+                                <?php p($l->t("Please, add a disclaimer file!"));?>
+                <form enctype="multipart/form-data" method="post" class="upload">
+                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <input type="submit" name="save" value="Upload" id="save" />
+                </form>
+                        </span>
+                <?php } ?>
+                <?php if((is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")) &&
+                                (md5_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf") === "54b3422935ea1305b2904bb6b906e095")){?>
+                        <span class="connectionwarning">
+                                <?php p($l->t("Please, change the default disclaimer file!"));?>
+                <form enctype="multipart/form-data" method="post" class="upload">
+                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <input type="submit" name="save" value="Upload" id="save" />
+                </form>
+                        </span>
+                <?php } ?>
+        </div>
 </form>
