e5b0192024a83383211bb51b59682dea17a88b39
Smiril <Smiril@users.noreply.github.com>
2016-11-23 12:52:16 +0100

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -4,7 +4,7 @@
                 <h2><?php p($l->t('Disclaimer'));?></h2>
                 <p><?php p($l->t('Current disclaimer path:')); ?> <a href="/apps/disclaimer/pdf/disclaimer.pdf" target="_blank"><?php echo realpath(dirname(__DIR__));?>/pdf/disclaimer.pdf</a></p>
                 <form enctype="multipart/form-data" method="post" class="upload">
-                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
                 <input type="submit" name="save" value="Upload" id="save" />
                 </form>
 
@@ -12,7 +12,7 @@
                         <span class="connectionwarning">
                                 <?php p($l->t("Please, add a disclaimer file!"));?>
                 <form enctype="multipart/form-data" method="post" class="upload">
-                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
                 <input type="submit" name="save" value="Upload" id="save" />
                 </form>
                         </span>
@@ -22,7 +22,7 @@
                         <span class="connectionwarning">
                                 <?php p($l->t("Please, change the default disclaimer file!"));?>
                 <form enctype="multipart/form-data" method="post" class="upload">
-                <label>Datei:</label><input type="file" name="userfile" id="file" size="46" /><br />
+                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
                 <input type="submit" name="save" value="Upload" id="save" />
                 </form>
                         </span>
