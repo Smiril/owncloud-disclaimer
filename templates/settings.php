3e47a01200ca3fb78eb15fb0eb1523e3688e61f5
Smiril <Smiril@users.noreply.github.com>
2016-11-23 22:01:03 +0100

    Add File Upload
    working on a simple file upload function for the disclaimer.pdf

--- templates/settings.php
+++ templates/settings.php
@@ -1,31 +1,48 @@
 <?php //use \OCA\disclaimer\disclaimer; ?>
-<form id="disclaimer">
+<form id="disclaimer" action="<?php $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="POST">
         <div class="section">
                 <h2><?php p($l->t('Disclaimer'));?></h2>
                 <p><?php p($l->t('Current disclaimer path:')); ?> <a href="/apps/disclaimer/pdf/disclaimer.pdf" target="_blank"><?php echo realpath(dirname(__DIR__));?>/pdf/disclaimer.pdf</a></p>
-                <form enctype="multipart/form-data" method="post" class="upload">
-                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
-                <input type="submit" name="save" value="Upload" id="save" />
-                </form>
 
                         <?php if(!is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){?>
                         <span class="connectionwarning">
                                 <?php p($l->t("Please, add a disclaimer file!"));?>
-                <form enctype="multipart/form-data" method="post" class="upload">
-                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
-                <input type="submit" name="save" value="Upload" id="save" />
-                </form>
                         </span>
                 <?php } ?>
-                <?php if((is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")) &&
+                <?php if((is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")) && 
                                 (md5_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf") === "54b3422935ea1305b2904bb6b906e095")){?>
                         <span class="connectionwarning">
                                 <?php p($l->t("Please, change the default disclaimer file!"));?>
-                <form enctype="multipart/form-data" method="post" class="upload">
-                <label><?php p($l->t("File:"))?></label><input type="file" name="userfile" id="file" size="46" /><br />
-                <input type="submit" name="save" value="Upload" id="save" />
-                </form>
                         </span>
                 <?php } ?>
         </div>
+
+<?php
+
+                $txt ='<label>File:</label><input name="protocoll" type="file"  size="46" /><br>
+                <input type="submit" value="submit" />';
+
+
+if(isset($_POST['disclaimer'])) {
+            if(isset($_POST['protocoll'])) {
+                        p($l->t("File transmission Success!"));
+                if(move_uploaded_file($_POST['protocoll']['tmp_name'], realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){
+                echo "<br>";
+                p($l->t("Upload Success"));
+                }
+            else{
+                echo "<br>";
+                p($l->t("Error on Upload"));
+                }
+
+               }
+            else {
+               p($l->t("File transmission Failed!"));
+            }
+         } 
+        else
+        {
+        echo $txt;
+        }
+?>
 </form>
