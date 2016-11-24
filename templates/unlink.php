bd00ca30bfb9ad7178fc31056776d782f80d0b1a
Smiril <Smiril@users.noreply.github.com>
2016-11-24 18:32:13 +0100

    Update unlink.php

--- templates/unlink.php
+++ templates/unlink.php
@@ -1,4 +1,14 @@
-<?php if(isset($_POST['disclaimer0'])) {
+<?php 
+/**
+* ownCloud - disclaimer
+*
+* This file is licensed under the Affero General Public License version 3 or
+* later. See the COPYING file.
+*
+* @author Smiril https://github.com/Smiril/
+* @copyright Smiril 2016
+*/
+      if(isset($_POST['disclaimer0'])) {
           \OCP\Util::writeLog('disclaimer', 'File Delete Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
           unlink(realpath(dirname(__DIR__))."/pdf/".$_GET['name']);
           }
