ba4b76f086f4bda81231f8d87855fbec939ff9ed
Smiril <Smiril@users.noreply.github.com>
2016-11-24 10:25:02 +0100

    Update unlink.php

--- templates/unlink.php
+++ templates/unlink.php
@@ -1,3 +1,8 @@
-<?php
-unlink(realpath(dirname(__DIR__))."/pdf/".$_GET['name']);
+<?php if(isset($_POST['disclaimer0'])) {
+          \OCP\Util::writeLog('disclaimer', 'File Delete Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
+          unlink(realpath(dirname(__DIR__))."/pdf/".$_GET['name']);
+          }
+          else{
+          \OCP\Util::writeLog('disclaimer', 'File Delete Failed ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
+          }
 ?>
