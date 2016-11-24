fd9a54116a52ffa1e6db271ec6bddc49dd431bbc
Smiril <Smiril@users.noreply.github.com>
2016-11-24 01:38:46 +0100

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -1,24 +1,7 @@
 <?php //use \OCA\disclaimer\disclaimer; ?>
-<h2><?php p($l->t('Disclaimer'));?></h2>
-<?php         if(isset($_POST['disclaimer'])) {
-                if(isset($_POST['protocoll'])) {
-                        p($l->t("File transmission Success!"));
-                                if(rename($_POST['protocoll']['tmp_name'], realpath(dirname(__DIR__)).'/pdf/disclaimer.pdfi')){
-                                echo "<br>";
-                                p($l->t("Upload Success"));
-                                }
-                                else{
-                                        echo "<br>";
-                                        p($l->t("Error on Upload"));
-                                }
-                        }
-                        else {
-                                p($l->t("File transmission Failed!"));
-                        }
-                }
-?>
-<form id="disclaimer" action="#disclaimer" method="POST">
+<form id="disclaimer">
         <div class="section">
+                <h2><?php p($l->t('Disclaimer'));?></h2>
                 <p><?php p($l->t('Current disclaimer path:')); ?> <a href="/apps/disclaimer/pdf/disclaimer.pdf" target="_blank"><?php echo realpath(dirname(__DIR__));?>/pdf/disclaimer.pdf</a></p>
 
                 <?php if(is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){?>
@@ -31,6 +14,12 @@
                 <?php if(!is_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf")){?>
                 <span class="connectionwarning">
                 <?php p($l->t("Please, add a disclaimer file!"));?>
+                <?php
+                $txt =' <form id="disclaimer1" action="/apps/disclaimer/templates/upload.php" enctype="multipart/form-data" method="POST">
+                        <p><label>File:</label><input name="protocoll" type="file" size="46"></p>
+                        <p><input type="submit" value="submit"></p></form>';
+                echo $txt;
+                ?>
                 </span>
                 <?php } ?>
 
@@ -38,13 +27,14 @@
                 (md5_file(realpath(dirname(__DIR__))."/pdf/disclaimer.pdf") === "54b3422935ea1305b2904bb6b906e095")){?>
                 <span class="connectionwarning">
                 <?php p($l->t("Please, change the default disclaimer file!"));?>
+                <?php
+                $txt =' <form id="disclaimer1" action="/apps/disclaimer/templates/upload.php" enctype="multipart/form-data" method="POST">
+                        <p><label>File:</label><input name="protocoll" type="file" size="46"></p>
+                        <p><input type="submit" value="submit"></p></form>';
+                echo $txt;
+                ?>
+
                 </span>
         <?php } ?>
         </div>
-<?php
-                $txt =' <p><label>File:</label><input name="protocoll" type="file" size="46" /></p>
-                        <p><input type="submit" value="submit" /></p>';
-                echo $txt;
-?>
 </form>
-
