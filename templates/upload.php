d15fcfc900df473c2b9a156de65e3a0f5be2cc23
Smiril <Smiril@users.noreply.github.com>
2016-11-24 01:53:59 +0100

    write log entry

--- templates/upload.php
+++ templates/upload.php
@@ -1,18 +1,15 @@
 <?php         if(isset($_POST['disclaimer1'])) {
                 if(isset($_FILES['protocoll'])) {
-                        p($l->t("File transmission Success!"));
+                        \OCP\Util::writeLog('disclaimer', 'File transmission Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                                 if(rename($_FILES['protocoll']['tmp_name'], realpath(dirname(__DIR__)).'/pdf/disclaimer.pdfi')){
-                                echo "<br>";
-                                p($l->t("Upload Success"));
+                                \OCP\Util::writeLog('disclaimer', 'Disclaimer Upload Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                                 }
                                 else{
-                                        echo "<br>";
-                                        p($l->t("Error on Upload"));
+                                \OCP\Util::writeLog('disclaimer', 'Disclaimer Upload Failed ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                                 }
                         }
                         else {
-                                p($l->t("File transmission Failed!"));
+                        \OCP\Util::writeLog('disclaimer', 'File transmission Failed' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                         }
                 }
 ?>
-
