3a1187cca35af89aa91e513264393805d50ef2bb
Smiril <Smiril@users.noreply.github.com>
2016-11-24 18:33:03 +0100

    Update upload.php

--- templates/upload.php
+++ templates/upload.php
@@ -1,4 +1,15 @@
-<?php         if(isset($_POST['disclaimer1'])) {
+<?php
+/**
+* ownCloud - disclaimer
+*
+* This file is licensed under the Affero General Public License version 3 or
+* later. See the COPYING file.
+*
+* @author Smiril https://github.com/Smiril/
+* @copyright Smiril 2016
+*/
+
+          if(isset($_POST['disclaimer1'])) {
                 if(isset($_FILES['protocoll'])) {
                         \OCP\Util::writeLog('disclaimer', 'File transmission Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                                 if(rename($_FILES['protocoll']['tmp_name'], realpath(dirname(__DIR__)).'/pdf/disclaimer.pdf')){
