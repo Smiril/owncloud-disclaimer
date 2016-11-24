d5fa2a6d044de3eec35f88caa52a95a3e3380620
Smiril <Smiril@users.noreply.github.com>
2016-11-24 09:38:55 +0100

    Update upload.php

--- templates/upload.php
+++ templates/upload.php
@@ -1,7 +1,7 @@
 <?php         if(isset($_POST['disclaimer1'])) {
                 if(isset($_FILES['protocoll'])) {
                         \OCP\Util::writeLog('disclaimer', 'File transmission Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
-                                if(rename($_FILES['protocoll']['tmp_name'], realpath(dirname(__DIR__)).'/pdf/disclaimer.pdfi')){
+                                if(rename($_FILES['protocoll']['tmp_name'], realpath(dirname(__DIR__)).'/pdf/disclaimer.pdf')){
                                 \OCP\Util::writeLog('disclaimer', 'Disclaimer Upload Success ' .date('Y-m-d H:i:s'), \OCP\Util::WARN);
                                 }
                                 else{
